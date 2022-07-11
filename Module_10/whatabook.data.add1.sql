bbaafbcc5c243c5a80666d85294c84aa84206876
Robert Behrens <rob.behrens1028@gmail.com>
2022-07-10 20:14:43 -0600

    this is the SQL code used tob build ann add data to the whatabook database.

---
+++ Module_10/whatabook.data.add1.sql
@@ -0,0 +1,27 @@
+USE whatabook;
+INSERT INTO store(store_locate)
+VALUES ('1080 L, ST Omaha NE, 68148');
+INSERT INTO user(first_name, last_name)
+VALUES ('Jack',"O'Neill");
+INSERT INTO user(first_name, last_name)
+VALUES ('Tom',"Dalton");
+INSERT INTO user(first_name, last_name)
+VALUES ('John',"Sheppard");
+INSERT INTO book(book_name, author)
+VALUES ("harry potter and the sorcer stone", "J.K Rowling");
+INSERT INTO book(book_name, author)
+VALUES ("harry potter and the chamber of secrets", "J.K Rowling");
+INSERT INTO book(book_name, author)
+VALUES ("harry potter and the prisoner of azkaban", "J.K Rowling");
+INSERT INTO book(book_name, author)
+VALUES ("harry potter and the goblet of fire", "J.K Rowling");
+INSERT INTO book(book_name, author)
+VALUES ("harry potter and the order of phoenix", "J.K Rowling");
+INSERT INTO book(book_name, author)
+VALUES ("harry potter and the half blood prince", "J.K Rowling");
+INSERT INTO book(book_name, author)
+VALUES ("harry potter and the deathly hallows", "J.K Rowling");
+INSERT INTO book(book_name, author)
+VALUES ("Legend", "Marie lu");
+INSERT INTO book(book_name, author)
+VALUES ("the hunger games", "Suzanne Collins");
---
+++ Module_10/whatabook.data.add2.sql
@@ -0,0 +1,8 @@
+USE whatabook;
+INSERT INTO wishlist(user_id, book_id)
+VALUES (2, 1);
+INSERT INTO wishlist(user_id, book_id)
+VALUES (1, 2);
+INSERT INTO wishlist(user_id, book_id)
+VALUES (3, 6);
+SELECT * FROM wishlist;
